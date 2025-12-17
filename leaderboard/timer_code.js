640c0a859e44f7ec5c4b74e7b5abe56a0d8b60cc
nstfleury <nstfleury@gmail.com>
2025-12-17 16:30:09 +0100

    Add files via upload

---
+++ leaderboard/timer_code.js
@@ -0,0 +1,16 @@
+const departMinutes = 1;
+        let temps = departMinutes * 60;
+
+        const timerElement = document.getElementById("timer");
+        const phrase = "temps restant"
+
+        setInterval(() => {
+        let minutes = parseInt(temps / 60, 10);
+        let secondes = parseInt(temps % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        secondes = secondes < 10 ? "0" + secondes : secondes;
+
+        timerElement.innerText =`${phrase}: ${minutes}:${secondes}`;
+        temps = temps <= 0 ? 0 : temps - 1;
+        }, 1000);
