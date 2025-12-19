9d2e99c114d7cd32e8370d92f785086a33a0e7c5
Fardine-K <fardine.koojah@gmail.com>
2025-12-19 16:19:45 +0100

    ajout de commentaires

--- jeu python/projet/linkfile.py
+++ jeu python/projet/linkfile.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template, request, jsonify
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__) #initialiser le serveur
 
 @app.route("/")
-def index():
+def index():   #ouvre le fichier html quand on ouvre le site
     return render_template("ecran_titre.html")
 
-@app.route("/start") #site home
-def start_game():
-    subprocess.Popen(["python", "reaction_game_v1.2.py"])
+@app.route("/start")
+def start_game():  #ouvre le jeu pygame quand on clique sur le bouton start du site
+    subprocess.Popen(["python", "reaction_game_v1.2.py"])  #subprocess permet de ne pas arrêter le fonctionnement du serveur pendant que le jeu se lance
     return "ok"
 
 @app.route("/score", methods=["POST"])
-def receive_score():
+def receive_score(): #recupere le score et le stocke dans le serveur
     global latest_score
     data = request.json
     latest_score = data["score"]
     return jsonify({"status": "ok"})
 
 @app.route("/score", methods=["GET"])
-def send_score():
+def send_score():  #envoie le score stocké ici au site web quand il est demandé
     return jsonify({"score": latest_score})
 
 
